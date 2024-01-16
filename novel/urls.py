9e0399419d77a6115003f183b903b02964d4df89
daniel <63087796+croisome@users.noreply.github.com>
2024-01-16 13:40:38 +0000

    add novel, chapter interfaces

--- novel/urls.py
+++ novel/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("novels/<int:n_id>/", views.get_novel_by_id)
+    path("novels/<int:book_id>/", views.get_book_info_by_id),
+    path("chapters/<int:book_id>/<int:start_index>", views.get_chapters_list_by_id)
 ]
-
--- novel/views.py
+++ novel/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-
+from django.forms.models import model_to_dict
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from .models import BookInfo, BookChapter
 
 
-def get_novel_by_id(request, n_id: int):
+def get_book_info_by_id(request, book_id: int):
+    try:
+        value = model_to_dict(BookInfo.objects.get(id=book_id))
+    except Exception as e:
+        print(e)
+        value = {}
+    return JsonResponse(value)
 
-    return HttpResponse("hello"+str(n_id))
+
+from django.core import serializers
+
+
+def get_chapters_list_by_id(request, book_id: int, start_index: int):
+    val_set = BookChapter.objects.filter(book_id=book_id)[start_index:100]
+    # print(val_set)
+    res = serializers.serialize("json", val_set)
+
+    return HttpResponse(res, content_type='application/json')
